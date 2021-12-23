8ec08b71a53d88b57f6e80590e975ed5dc581f04
Tasya Lailinissa D <tasya.lailinissa@gmail.com>
2021-12-23 14:09:23 +0700

    About API

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,5 @@
 export const SUBTITLE_URL = 'https://api.chucknorris.io/jokes/random'
 export const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos'
+export const ABOUT_URL = 'https://swapi.dev/api/films/1'
+export const ABOUT_BACKUP_URL = 'https://www.swapi.tech/api/films/1'
+
