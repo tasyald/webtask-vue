80d09da62371519604f45532fedee74eb9968419
Tasya Lailinissa D <tasya.lailinissa@gmail.com>
2021-12-23 15:23:33 +0700

    Contact info component

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="contact-info column is-7">
+    <customer-support number="1-500-445" />
+    <contact-social />
+  </div>
+</template>
+
+<script>
+import ContactSocial from '@/components/ContactSocial.vue'
+import CustomerSupport from '@/components/CustomerSupport.vue'
+
+export default {
+  name: 'ContactInfo',
+  components: {
+    CustomerSupport,
+    ContactSocial
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/base';
+
+.contact-info {
+  padding: 3rem;
+  text-align: left;
+}
+
+@media screen and (max-width: 768px) {
+  .contact-info {
+    display: none;
+  }
+}
+</style>
